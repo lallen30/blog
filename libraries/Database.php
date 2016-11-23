a1add21c90ebc3eb799b17693030896f99c9f141
Larry Allen <lallen30@yahoo.com>
2016-11-22 22:55:45 -0500

    added select, insert, update and delete functions to database class

--- libraries/Database.php
+++ libraries/Database.php
@@ -26,4 +26,64 @@ class Database{
              return false;
          }
      }
+
+     /*
+      * Select
+      */
+
+      public function select($query){
+          $result = $this->link->query($query) or die($this->link->error.__LINE__);
+          if($result->num_rows > 0){
+              return $result;
+          }else{
+              return false;
+          }
+      }
+
+      /*
+       * Insert
+       */
+       public function insert($query){
+           $insert_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
+           //Validate insert
+           if($insert_row){
+               header("Location: index.php?msg=".urlencode('Record Added'));
+               exit();
+           }else{
+               die('Error : ('. $this->link->errno .')' . $this->link->error);
+           }
+       }
+
+              /*
+               * Update
+               */
+               public function update($query){
+                   $update_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
+                   //Validate update
+                   if($update_row){
+                       header("Location: index.php?msg=".urlencode('Record Updated'));
+                       exit();
+                   }else{
+                       die('Error : ('. $this->link->errno .')' . $this->link->error);
+                   }
+               }
+
+
+              /*
+               * Delete
+               */
+               public function delete($query){
+                   $delete_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
+                   //Validate delete
+                   if($delete_row){
+                       header("Location: index.php?msg=".urlencode('Record Deleted'));
+                       exit();
+                   }else{
+                       die('Error : ('. $this->link->errno .')' . $this->link->error);
+                   }
+               }
+
 }
